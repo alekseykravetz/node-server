295bf94a807d3ac38c213781297a091fc205fff0
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2019-06-18 18:15:40 +0300

    added users api

--- api/public.api.js
+++ api/public.api.js
@@ -1,7 +1,16 @@
 const express = require('express');
 
+const dataAccesss = require('../data-access');
+
+
 const router = express.Router();
 
+router.get('/api/v1/users', async (req, res) => {
+
+    const users = await dataAccesss.getUsers();
+
+    res.json(users);
+});
 
 router.get('/api/v1/user/:id/:name', async (req, res) => {
     const { id, name } = req.params;
